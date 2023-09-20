5f950f5fd7b40e90da4e5a26a194443fe65f4d64
Ludvik Jerabek <ljerabek@proofpoint.com>
2023-09-19 20:54:45 -0400

    Update PageIterator.py

--- src/psat_api/web/PageIterator.py
+++ src/psat_api/web/PageIterator.py
@@ -33,49 +33,60 @@ class PageIterator:
     def __next__(self) -> List:
         if self.__first_request:
             self.__first_request = False
-        elif self.get_next() is not None:
-            self.__response = self.__session.get(self.get_next())
+        elif self.next is not None:
+            self.__response = self.__session.get(self.next)
         else:
             raise StopIteration
         return self.__response.json().get('data', [])
 
-    def get_self(self) -> str:
+    @property
+    def self(self) -> str:
         url = self.__response.json().get('links', {}).get('self', None)
         return (urljoin(self.__response.url, url), None)[url is None]
 
-    def get_first(self) -> str:
+    @property
+    def first(self) -> str:
         url = self.__response.json().get('links', {}).get('first', None)
         return (urljoin(self.__response.url, url), None)[url is None]
 
-    def get_last(self) -> str:
+    @property
+    def last(self) -> str:
         url = self.__response.json().get('links', {}).get('last', None)
         return (urljoin(self.__response.url, url), None)[url is None]
 
-    def get_next(self) -> str:
+    @property
+    def next(self) -> str:
         url = self.__response.json().get('links', {}).get('next', None)
         return (urljoin(self.__response.url, url), None)[url is None]
 
-    def get_current_page_number(self) -> int:
+    @property
+    def current_page_number(self) -> int:
         return int(self.__response.json().get('meta', {}).get('page_number', 0))
 
-    def get_page_size(self) -> int:
+    @property
+    def page_size(self) -> int:
         return int(self.__response.json().get('meta', {}).get('page_size', 0))
 
-    def get_last_page_number(self) -> int:
-        if self.get_page_size() == 0:
+    @property
+    def last_page_number(self) -> int:
+        if self.page_size == 0:
             return 0
-        if self.get_record_count() == 0:
+        if self.record_count == 0:
             return 1
-        return int((self.get_record_count() + self.get_page_size() - 1) / self.get_page_size())
+        return int((self.record_count + self.page_size - 1) / self.page_size)
 
-    def get_record_count(self) -> int:
+    @property
+    def record_count(self) -> int:
         return int(self.__response.json().get('meta', {}).get('count', 0))
 
-    def get_status(self) -> int:
+    @property
+    def status(self) -> int:
         return self.__response.status_code
 
-    def get_reason(self) -> str:
+    @property
+    def reason(self) -> str:
         return self.__response.reason
 
-    def get_response(self) -> Response:
+    @property
+    def response(self) -> Response:
         return self.__response
